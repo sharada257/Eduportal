2ed993211d15f573e75a79cf4dfb3fd4e589c6e9
Sharada Kumari V <sharadav257@gmail.com>
2025-07-04 11:00:20 +0530

    Added admin.py files

--- backend/assessment/urls.py
+++ backend/assessment/urls.py
@@ -2,13 +2,14 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AssignmentViewSet, QuizViewSet, TestViewSet, SubmissionViewSet
+from .views import AssignmentViewSet, QuizViewSet, TestViewSet, SubmissionViewSet,SubjectTeacherSectionViewSet
 
 router = DefaultRouter()
 router.register(r'assignments', AssignmentViewSet)
 router.register(r'quizzes', QuizViewSet)
 router.register(r'tests', TestViewSet)
 router.register(r'submissions', SubmissionViewSet)
+router.register(r'subject-teacher-sections', SubjectTeacherSectionViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- backend/department/urls.py
+++ backend/department/urls.py
@@ -1,12 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import DepartmentViewSet, SectionViewSet, SubjectViewSet, SubjectTeacherSectionViewSet
+from .views import DepartmentViewSet, SectionViewSet, SubjectViewSet
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet)
 router.register(r'sections', SectionViewSet)
 router.register(r'subjects', SubjectViewSet)
-router.register(r'subject-teacher-sections', SubjectTeacherSectionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
